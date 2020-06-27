f81ef6c0f5f8d7d498e0b51da7254f1fa7ec455b
Allan Cheng <allanhfcheng@gmail.com>
2020-06-27 00:53:50 -0700

    stuff

---
+++ ListTest.js
@@ -0,0 +1,29 @@
+var LinkedList= require('./LinkedList.js');
+/*
+function display(ls){
+    let currNode = ls.head;
+
+   // while(currNode !==null)
+    {
+        console.log(currNode.head.value)
+        currNode = ls.next;
+        
+        console.log(ls.head.value)
+    }
+}
+*/
+function arrayStuff(){
+
+ 
+
+    // Create an instance of the Array class
+    let ls = new LinkedList();
+
+    // Add an item to the array
+ ls.insertFirst("Apollo");
+ 
+ console.log(ls.find("Apollo"));
+ //display(ls);
+}
+
+arrayStuff()
